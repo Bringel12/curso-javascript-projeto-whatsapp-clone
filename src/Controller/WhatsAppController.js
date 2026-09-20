5a8d1d2c2512140d0cc1a095760f6adfd1b82639
Bringel12 <matheushenriquebringelferreira@gmail.com>
2026-09-20 15:20:17 -0300

    Atualização

--- src/Controller/WhatsAppController.js
+++ src/Controller/WhatsAppController.js
@@ -4,6 +4,7 @@ class WhatsAppController {
 
         this.elementsPrototype();
         this.loadElements();
+        this.initEvents();
     }
 
     loadElements() {
@@ -66,5 +67,38 @@ class WhatsAppController {
             return  this.classList.contains(name);
 
     }
+    
 }
+initEvents(){
+    this.el.myPhoto.on('click', e=>{
+        this.closeAllLeftPanel();
+        this.el.panelEditProfile.show();
+        setTimeout(()=>{
+        this.el.panelEditProfile.addClass('open');
+
+        },300);
+    });
+    this.el.btnNewContact.on('click', e=>{
+        this.closeAllLeftPanel(); 
+        this.el.panelAddContact.show();
+        setTimeout(()=>{
+            this.el.panelAddContact.addClass('open');
+
+        }, 300);
+    });
+    this.el.btnClosePanelEditProfile.on('click', e=>{
+        this.el.panelEditProfile.removeClass('open');
+    });
+
+    this.el.btnClosePanelAddContact.on('click', e=>{
+     this.el.panelAddContact.removeClass('open');
+    });
+
+}
+
+closeAllLeftPanel(){
+    this.el.panelAddContact.hide();
+    this.el.panelEditProfile.hide();
+}
+
 }
